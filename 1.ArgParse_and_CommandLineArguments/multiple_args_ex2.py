4e3f98bec0296bbd55ba53b2d21b3a3462e54f5b
Deepeksh <deepeksh77673@gmail.com>
2022-01-07 16:56:02 +0530

    more argparse examples

---
+++ 1.ArgParse_and_CommandLineArguments/multiple_args_ex2.py
@@ -0,0 +1,15 @@
+import argparse
+import cv2
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--input", required=True,
+	help="path to input image")
+ap.add_argument("-o", "--output", required=True,
+	help="path to output image")
+args = vars(ap.parse_args())
+# load the input image from disk
+image = cv2.imread(args["input"])
+
+cv2.imshow(args["output"], image)
+cv2.waitKey(0)
+
